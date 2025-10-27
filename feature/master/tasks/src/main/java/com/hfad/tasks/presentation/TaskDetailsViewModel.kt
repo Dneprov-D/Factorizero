262a5056ed9d5898afbd59b6ad2338b9a3e3a446
D.Dneprov <ds.dnepr@mail.ru>
2025-10-27 12:42:33 +0300

    done count is updating in TaskDetailsScreen

--- feature/master/tasks/src/main/java/com/hfad/tasks/presentation/TaskDetailsViewModel.kt
+++ feature/master/tasks/src/main/java/com/hfad/tasks/presentation/TaskDetailsViewModel.kt
@@ -9,11 +9,20 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import com.hfad.data.repository.LoginRepository
+import com.hfad.model.WorkTask
 import com.hfad.navigation.Screen
 import com.hfad.ui.profile.uimodel.TaskUiModel
+import com.hfad.ui.profile.uimodel.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -35,35 +44,51 @@ class TaskDetailsViewModel @Inject constructor(
                 quantity = args.quantity,
                 doneCount = 0,
                 done = false
-            )
+            ),
+            tasksList = emptyList()
         )
     )
         private set
 
     init {
-        loadTask()
+        observeTask()
     }
 
-    fun loadTask() {
-        loginRepository.loadTask(
-            key = state.task.key,
-            onLoadSuccess = { workTask ->
-                state = state.copy(
-                    task = state.task.copy(
-                        title = workTask.title,
-                        quantity = workTask.quantity,
-                        doneCount = workTask.doneCount,
-                        done = workTask.done
+    private fun observeTask() {
+        viewModelScope.launch {
+            activeTaskFlow()
+                .collect { task ->
+                    state = state.copy(
+                        task = task
                     )
-                )
-            },
-            onLoadFailure = { error ->
-                Log.e("Firebase", "Error loading task: $error")
-                state = state.copy(
-                    task = state.task.copy(doneCount = args.doneCount)
-                )
+                }
+        }
+    }
+
+    private fun activeTaskFlow():
+            Flow<TaskUiModel> = callbackFlow {
+        val db = Firebase.firestore
+        val listenerRegistration = db.collection("tasks")
+            .document(state.task.key)
+            .addSnapshotListener { snapshot, error ->
+                error?.let {
+                    close(it)
+                    return@addSnapshotListener
+                }
+
+                snapshot?.let { document ->
+                    if (document.exists()) {
+                        val workTask = document.toObject(WorkTask::class.java)
+                        workTask?.let {
+                            trySend(it.toUiModel())
+                        }
+                    }
+                }
             }
-        )
+
+        awaitClose {
+            listenerRegistration.remove()
+        }
     }
 
     fun closeTask() {
@@ -79,10 +104,13 @@ class TaskDetailsViewModel @Inject constructor(
             }
         )
     }
+
     sealed interface NavigationEvent {
         data object OnClosed : NavigationEvent
     }
+
     data class TaskDetailsScreenState(
-        val task: TaskUiModel
+        val task: TaskUiModel,
+        val tasksList: List<TaskUiModel>
     )
 }
